582054e73aa521ee2bfdba55508ae01dd0ea577e
Suraj-000 <01fe20bec246@kletech.ac.in>
2022-07-29 11:48:52 +0530

    add a project

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -10,7 +10,14 @@ function ProjectsPage(){
     <p>my projects</p>
   </div>
   <div class="projects-solution">
-    <p>my projects will be added soon.</p>
+    <div className='project-1'>
+        <div className='project-header'>
+        <a href="https://shielded-peak-61835.herokuapp.com/">Blood Donation site: </a>
+        <p>This is a prototype of blood donation site build for the understand of CRUD in react.js.</p>
+        </div>
+        <div class="linkedin link"><a href="https://shielded-peak-61835.herokuapp.com/" target="_blank"><img src="images/website.svg" alt="website" /></a></div>
+        <div class="github link"><a href="https://github.com/Suraj-000/blood_donation_site" target="_blank"><img src="images/github.svg" alt="github" /></a></div>
+    </div>
   </div>
 </section>
   )
